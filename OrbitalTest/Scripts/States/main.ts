2abd4f9321fcc69224c188c389ea4196459748b6
Andrey Kurosh <mail@spark.ms>
2014-10-31 15:50:36 +0300

    Testy.

---
+++ OrbitalTest/Scripts/States/main.ts
@@ -0,0 +1,31 @@
+﻿class MainState extends Phaser.State {
+    
+    rocket: Rocket;
+
+    preload() {
+        this.load.image('rocket2', 'Content/Sprites/rocket2.png');
+    }
+
+    create() {
+        this.rocket = new Rocket(this.game, 320, 800);
+        var isThrusting = false;
+
+        this.input.mouse.onMouseDown = (evt) => {
+            this.rocket.thrustTo(evt.clientX, evt.clientY);
+            isThrusting = true;
+        };
+
+        this.input.mouse.onMouseUp = (evt) => {
+            this.rocket.stop();
+            isThrusting = false;
+        };
+
+        this.input.mouse.onMouseMove = (evt) => {
+            if (isThrusting) {
+                this.rocket.thrustTo(evt.clientX, evt.clientY);
+            } else {
+                this.rocket.stop();
+            }
+        };
+    }
+} 
---
+++ OrbitalTest/Scripts/orbital.ts
@@ -0,0 +1,15 @@
+﻿class OrbitalGame extends Phaser.Game {
+    // -----------------------
+    // Constructor
+    // -----------------------
+    constructor() {
+        super(640, 960, Phaser.AUTO);
+
+        this.state.add('Main', MainState, false);
+        this.state.start('Main');
+    }
+}
+
+window.onload = () => {
+    var game = new OrbitalGame();
+}
