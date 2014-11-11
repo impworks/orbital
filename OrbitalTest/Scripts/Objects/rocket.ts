7e9544d2e93205db479dcd0144bf2f96247841b1
Andrey Kurosh <mail@spark.ms>
2014-11-11 15:03:14 +0300

    Orbits flushed from prototype #1. Added some planets.

--- OrbitalTest/Scripts/Objects/rocket.ts
+++ OrbitalTest/Scripts/Objects/rocket.ts
@@ -15,41 +15,5 @@
     private velocity: Phaser.Point;
 
     update() {
-//        if (this.destination) {
-//            this.momentum = new Phaser.Point(
-//                this.destination.x - this.position.x,
-//                this.destination.y - this.position.y
-//            );
-//
-//            if (this.momentum.getMagnitude() < 30) {
-//                this.momentum.setMagnitude(0);
-//            } else {
-//                this.momentum.normalize();
-//            }
-//        } else if(!this.momentum.isZero()) {
-//            this.momentum = new Phaser.Point();
-//        }
-//
-//        this.position.add(this.velocity.x, this.velocity.y);
-//        this.velocity.add(this.momentum.x, this.momentum.y);
-//
-//        if(!this.momentum.isZero())
-//            this.rotation = -(Math.atan2(this.velocity.x, this.velocity.y) - (Math.PI / 2));
-//
-//        if (this.velocity.getMagnitude() > 15)
-//            this.velocity.setMagnitude(15);
-//
-//        if (this.momentum.isZero() && !this.velocity.isZero()) {
-//            var threshold = 0.5;
-//            var dec = 1;
-//            var mag = this.velocity.getMagnitude();
-//
-//            if (Math.abs(mag) < threshold)
-//                mag = 0;
-//            else
-//                mag -= mag > 0 ? dec : -dec;
-//
-//            this.velocity.setMagnitude(mag)
-//        }
     }
 } 
--- OrbitalTest/Scripts/States/MainState.ts
+++ OrbitalTest/Scripts/States/MainState.ts
@@ -1,5 +1,9 @@
 ﻿class MainState extends Phaser.State {
 
+    // -----------------------
+    // Constructor
+    // -----------------------
+
     constructor() {
         super();
     }
@@ -13,6 +17,12 @@
     planets: Planet[];
     plasma: Plasma;
 
+    orbit: IOrbit;
+
+    // -----------------------
+    // Resource loading
+    // -----------------------
+
     preload() {
         var assets = [
             'rocket',
@@ -25,10 +35,44 @@
         assets.forEach(name => this.load.image(name, 'Content/Sprites/' + name + '.png'));
     }
 
+    // -----------------------
+    // Initialization
+    // -----------------------
+
     create() {
         this.bg = new SkyBackground(this.game);
         this.rocket = new Rocket(this.game, OrbitalGame.SCREEN_WIDTH / 2, OrbitalGame.SCREEN_HEIGHT / 3 * 2);
+        this.planets = this.createPlanets();
+        this.orbit = this.initRocketOrbit();
 
-        // todo: create stuff
+        // todo: subscribe to events
     }
-} 
+
+    createPlanets(): Planet[] {
+        return [
+            new Planet(this.game, 300, 300),
+            new Planet(this.game, 700, 400)
+        ];
+    }
+
+    initRocketOrbit(): IOrbit {
+        var p = this.planets[0];
+        var r = this.rocket;
+        var distance = Math.sqrt(Math.pow(p.position.x - r.position.x, 2) + Math.pow(p.position.y - r.position.y, 2));
+        return {
+            planet: p,
+            radius: distance
+        };
+    }
+
+    // -----------------------
+    // Game logic
+    // -----------------------
+
+    // todo
+}
+
+interface IOrbit {
+    planet: Planet;
+    radius: number;
+}
