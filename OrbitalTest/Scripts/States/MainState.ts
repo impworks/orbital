7eceadb29d1eb8e70970f061cb1ef4ad025de77c
Andrey Kurosh <mail@spark.ms>
2014-11-11 16:33:17 +0300

    Rotating rocket.

--- OrbitalTest/Scripts/States/MainState.ts
+++ OrbitalTest/Scripts/States/MainState.ts
@@ -58,21 +58,55 @@
     initRocketOrbit(): IOrbit {
         var p = this.planets[0];
         var r = this.rocket;
-        var distance = Math.sqrt(Math.pow(p.position.x - r.position.x, 2) + Math.pow(p.position.y - r.position.y, 2));
+        var radius = Math.sqrt(Math.pow(p.position.x - r.position.x, 2) + Math.pow(p.position.y - r.position.y, 2));
+        var angle = Math.atan2(1, 2);
+        var speed = 3;
+
         return {
             planet: p,
-            radius: distance
+            radius: radius,
+            speed: speed,
+            angle: angle,
+            angleIncrement: this.calculateAngleIncrement(radius, speed)
         };
+    }
+
+    calculateAngleIncrement(radius: number, speed: number) : number {
+        var len = radius * 2 * Math.PI;
+        return speed / len * Math.PI * 2;
     }
 
     // -----------------------
     // Game logic
     // -----------------------
 
-    // todo
+    update() {
+        this.moveRocket();
+    }
+
+    moveRocket() {
+        this.orbit.angle += this.orbit.angleIncrement;
+
+        var newPos = {
+            x: this.orbit.planet.position.x + Math.cos(this.orbit.angle) * this.orbit.radius,
+            y: this.orbit.planet.position.y + Math.sin(this.orbit.angle) * this.orbit.radius
+        };
+
+        var dist = {
+            x: newPos.x - this.rocket.position.x,
+            y: newPos.y - this.rocket.position.y,
+        };
+
+        this.rocket.rotation = -(Math.atan2(dist.x, dist.y) - (Math.PI / 2));
+        this.rocket.position.x = newPos.x;
+        this.rocket.position.y = newPos.y;
+    }
 }
 
 interface IOrbit {
     planet: Planet;
     radius: number;
+    speed: number;
+    angle: number;
+    angleIncrement: number;
 }
