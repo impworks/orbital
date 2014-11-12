59bc3e4a6abae471d58a189c3fff3906566f80cf
Andrey Kurosh <mail@spark.ms>
2014-11-12 13:09:31 +0300

    Hover effects & fixed sprites.

--- OrbitalTest/Scripts/Objects/Planet.ts
+++ OrbitalTest/Scripts/Objects/Planet.ts
@@ -16,6 +16,8 @@
         this.inputEnabled = true;
 
         this.events.onInputDown.add(() => this.onClicked());
+        this.events.onInputOver.add(() => this.onMouseOver());
+        this.events.onInputOut.add(() => this.onMouseOut());
 
         this.isSelected = false;
     }
@@ -24,12 +26,35 @@
     // Fields
     // -----------------------
 
-    isSelected: boolean;
-    gravity: number;
+    private _isSelected: boolean;
+    get isSelected(): boolean {
+        return this._isSelected;
+    }
+    set isSelected(value: boolean) {
+        if (value != this.isSelected) {
+            this._isSelected = value;
+            this.loadTexture(value ? 'planet-selected' : 'planet-any', 0);
+            this.anchor.setTo(0.5, 0.5);
+        }
+    }
 
     onClicked() {
         if (this.state.orbit.planet != this) {
             this.state.setRocketOrbit(this);
+        }
+    }
+
+    onMouseOver() {
+        if (!this.isSelected) {
+            this.loadTexture('planet-hover', 0);
+            this.anchor.setTo(0.5, 0.5);
+        }
+    }
+
+    onMouseOut() {
+        if (!this.isSelected) {
+            this.loadTexture('planet-any', 0);
+            this.anchor.setTo(0.5, 0.5);
         }
     }
 } 
